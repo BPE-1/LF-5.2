0469b99651eca3fd8aad693bf4c31e80ca94b210
Bilal Patrick Eichert <143252581+BPE-1@users.noreply.github.com>
2024-11-12 11:32:23 +0100

    …

---
+++ Python Basics/Python Basics Aufgabe 12.py
@@ -0,0 +1,28 @@
+"""
+Wir besuchen die Insel der Ritter und Schurken. Ritter sagen stets die Wahrheit, Schurken lügen immer. Jeder Einwohner ist
+entweder Ritter oder Schurke.
+a. Warum kann kein einziger der Einwohner von sich behaupten: “Ich bin ein Schurke.”?
+"""
+Schurken lügen immer, somit sagen Schurken immer sie seien Ritter.
+Ritter sagen immer die Wahrheit, somit sagen Ritter immer sie seien Ritter.
+
+
+"""
+b. Ein Volkszähler befragt ein Ehepaar: “Wer von Ihnen ist Ritter, und wer ist Schurke?”
+Der Mann antwortet: “Wir sind beide Schurken.”.
+Ordnen Sie den beiden Befragten den richtigen Typ (Ritter oder Schurke) zu.
+"""
+Ehemann ist Schurke, Ehefrau Ritter.
+Schurken lügen immer, somit können nicht beide Schurken sein und seine Frau muss eine Ritterin sein.
+
+
+
+
+
+"""
+c. Ein Volkszähler befragt ein Ehepaar: “Wer von Ihnen ist Ritter und wer ist Schurke?”.
+Der Mann antwortet: “Mindestens einer von uns ist Schurke.”
+Ordnen Sie den beiden Befragten den richtigen Typ (Ritter oder Schurke) zu.
+"""
+Ehemann Ritter, Ehefrau Schurke.
+Ehemann kann kein Schurke sein, sonst wäre seine Aussage eine Lüge und keiner wäre ein Schurke.
